09a900246996b84750a0b955634e6af2796ff2d5
jonatao37 <jonathan.almeida@sptech.school>
2024-06-02 12:43:02 -0300

    Criando a a box do cadastro

--- src/BeforeLogin/Js/cadastroColaboradorscript.js
+++ src/BeforeLogin/Js/cadastroColaboradorscript.js
@@ -1,3 +1,17 @@
 document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
+    const sidebar = document.getElementById('sidebar');
+    const main = document.querySelector('main');
+    
+    sidebar.classList.toggle('open-sidebar');
+    main.classList.toggle('expanded');
 });
+
+const inputIcon = document.querySelector(".input__icon");
+const inputPassword = document.getElementById("password");
+
+inputIcon.addEventListener("click", () => {
+  inputIcon.classList.toggle("ri-eye-off-line");
+  inputIcon.classList.toggle("ri-eye-line");
+  inputPassword.type = inputPassword.type === "password" ? "text" : "password";
+});
+
