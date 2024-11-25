b19c110b27054da5cae4290cfaa95a74ab40e594
jonatao37 <jonathan.almeida@sptech.school>
2024-11-25 15:13:01 -0300

    Menu responsivo

--- src/AfterLogin/Js/funcoesPadroes.js
+++ src/AfterLogin/Js/funcoesPadroes.js
@@ -5,4 +5,18 @@ document.getElementById('open_btn').addEventListener('click', function () {
     sidebar.classList.toggle('open-sidebar');
     main.classList.toggle('expanded');
   });
+
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const menuOverlay = document.getElementById("menu-overlay");
+  
+  hamburgerMenu.addEventListener("click", () => {
+      menuOverlay.classList.toggle("open");
+  });
+  
+  // Fechar o menu ao clicar fora
+  document.addEventListener("click", (e) => {
+      if (!menuOverlay.contains(e.target) && !hamburgerMenu.contains(e.target)) {
+          menuOverlay.classList.remove("open");
+      }
+  });
   
